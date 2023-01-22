26825db3b38a6dd7a8423abcd9e37a485a392301
rocetDim <dmitriydem81@mail.ru>
2023-01-22 20:01:49 +0300

    Проблема с JS

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ MENU_LINKS.forEach(link => link.addEventListener('click', event => {
         behavior: 'smooth'
     });
 }));
+
+const HERO_DOTS = document.querySelectorAll('.hero__dot');
+const HERO_IMAGE = document.querySelectorAll('.hero__image');
+const HERO_TITLE = document.querySelectorAll('.hero__title');
+const HERO_TITLES = [
+    'Разработано 2000+ приложений',
+    'Бесплатные приложения',
+    'Тысячи довольных клиентов'
+]
+
+HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', () => {
+    HERO_DOTS.forEach(el => {
+        el.classList.remove('active');
+    });
+
+    dot.classList.add('active');
+    HERO_IMAGE.src = 'Component/hero_${index + 1}.jpg';
+    HERO_TITLE.textContent = HERO_TITLES[index];
+}));
