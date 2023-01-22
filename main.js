ad5a2b647dc2b7c1c86a3fdef1dff53e6510dbe8
rocetDim <dmitriydem81@mail.ru>
2023-01-22 21:23:40 +0300

    JS готов

--- main.js
+++ main.js
@@ -8,21 +8,33 @@ MENU_LINKS.forEach(link => link.addEventListener('click', event => {
     });
 }));
 
+// Slider
 const HERO_DOTS = document.querySelectorAll('.hero__dot');
-const HERO_IMAGE = document.querySelectorAll('.hero__image');
-const HERO_TITLE = document.querySelectorAll('.hero__title');
+const HERO_IMAGE = document.querySelector('.hero__image');
+const HERO_TITLE = document.querySelector('.hero__title');
+const HERO_SUBTITLE = document.querySelector('.hero__subtitle');
+
 const HERO_TITLES = [
     'Разработано 2000+ приложений',
     'Бесплатные приложения',
     'Тысячи довольных клиентов'
-]
+];
 
-HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', () => {
+const HERO_SUBTITLES = [
+    'Для вашего удобства мы разработали множество приложений для вашего бизнеса и команды, которые доступны в любое время суток',
+    'К любому сервисному обслуживанию мы подключим советы по управлению компанией совершенно бесплатно',
+    'Накопленный нами опыт позволяет предлагать клиентам обширный спектр качественных приложений и сервисов'
+  ];
+
+
+HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', event => {
     HERO_DOTS.forEach(el => {
         el.classList.remove('active');
     });
 
     dot.classList.add('active');
-    HERO_IMAGE.src = 'Component/hero_${index + 1}.jpg';
+
+    HERO_IMAGE.src = `Component/hero_${index + 1}.jpg`;
     HERO_TITLE.textContent = HERO_TITLES[index];
+    HERO_SUBTITLE.textContent = HERO_SUBTITLES[index];
 }));
